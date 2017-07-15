e2994786411c9cf89ad6adcf01dabf426b84e157
r-sniper <rshahshah2890@gmail.com>
2017-07-15 21:57:05 +0530

    Seperated modules

---
+++ General/models.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+
+# Create your models here.
+
+class Branch(models.Model):
+    branch_name = models.CharField(max_length=10)
+    HOD = models.OneToOneField()
+
+
+class Year(models.Model):
+    branch = models.ForeignKey(Branch)
+    year = models.CharField(max_length=2)
--- Student/models.py
+++ Student/models.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from django.db import models
 
 
@@ -9,27 +12,25 @@ def user_directory_path(instance, filename):
     return 'user_{0}/{1}'.format(instance.gr_number, filename)
 
 
+class StudentMaster(models.Model):
+    student = models.OneToOneField(StudentDetails)
 
 
-
-class Faculty(models.Model):
+class StudentDetails(models.Model):
+    # Basic Details
     first_name = models.CharField(max_length=100)
-    middle_name = models.CharField(max_length=100, blank=True)
+    middle_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     DOB = models.DateField(default='1996-02-11')
-
-    # account details
-    salary = models.IntegerField(default=10)
-
-    # teaching
-    teaching_experience = models.PositiveIntegerField(default=0)
-    subjects_experience = models.CharField(max_length=250)
-    projects = models.TextField(max_length=300)
+    admission_type = models.CharField(max_length=50)
+    shift = models.CharField(max_length=1)
+    caste_type = models.CharField(max_length=20)
+    branch = models.CharField(max_length=50)
+    gr_number = models.AutoField(primary_key=True)
 
     # personal details
-    caste_type = models.CharField(max_length=20)
     email = models.EmailField(max_length=100)
-    mobile = models.BigIntegerField(default="0")
+    mobile = models.BigIntegerField(default=0)
     religion = models.CharField(max_length=20)
     sub_caste = models.CharField(max_length=30)
     handicapped = models.BooleanField(default=0)
@@ -46,20 +47,20 @@ class Faculty(models.Model):
     father_name = models.CharField(max_length=50)
     father_profession = models.CharField(max_length=30)
     father_designation = models.CharField(max_length=30)
-    father_mobile = models.BigIntegerField(blank=True)
-    father_email = models.EmailField(blank=True)
+    father_mobile = models.BigIntegerField(default=0)
+    father_email = models.EmailField()
     # mother
     mother_name = models.CharField(max_length=50)
     mother_profession = models.CharField(max_length=30)
     mother_designation = models.CharField(max_length=30)
-    mother_mobile = models.BigIntegerField()
-    mother_email = models.EmailField(null=True, blank=True)
+    mother_mobile = models.BigIntegerField(default=0)
+    mother_email = models.EmailField(null=True)
 
     # permanent address
     permanent_address = models.CharField(max_length=100)
     permanent_state = models.CharField(max_length=50)
     permanent_city = models.CharField(max_length=50)
-    permanent_pin_code = models.IntegerField()
+    permanent_pin_code = models.DecimalField(default=000)
     permanent_country = models.CharField(max_length=50)
 
     # current address
@@ -69,12 +70,20 @@ class Faculty(models.Model):
     current_pin_code = models.IntegerField()
     current_country = models.CharField(max_length=50)
 
+    # Exam details
+    jee_physics = models.PositiveIntegerField(default=0)
+    jee_maths = models.PositiveIntegerField(default=0)
+    jee_chemistry = models.PositiveIntegerField(default=0)
+    jee_total = models.PositiveIntegerField(default=0)
+    jee_max_physics = models.PositiveIntegerField(default=0)
+    jee_max_maths = models.PositiveIntegerField(default=0)
+    jee_max_chemistry = models.PositiveIntegerField(default=0)
+
     # documents
+    doc_tenth_marksheet = models.FileField(upload_to=user_directory_path)
+    doc_twelfth_marksheet = models.FileField(upload_to=user_directory_path)
+    doc_jee_marksheet = models.FileField(upload_to=user_directory_path)
+    doc_profile_pic = models.ImageField(upload_to=user_directory_path)
 
-    doc = models.FileField(upload_to=user_directory_path)
-    doc_profile_pic = models.FileField(upload_to=user_directory_path)
-
-
-
-
-
+    def __str__(self):
+        return self.first_name + ' ' + str(self.pk)
