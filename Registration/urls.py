ef8bf135a1ef9b0c5527b8e4d8dfb9c796a763de
r-sniper <rshahshah2890@gmail.com>
2017-07-22 22:42:18 +0530

    Ignore

--- Registration/urls.py
+++ Registration/urls.py
@@ -5,9 +5,9 @@ from . import views
 urlpatterns = [
     # /register/student - Register Student
     url(r'^student/$', views.register_student, name='register_student'),
-    # /register/teacher - Register Student
-    url(r'^teacher/$', views.register_faculty, name='register_faculty'),
-    # /register/subject - Register Student
+    # /register/faculty - Register Faculty
+    url(r'^faculty/$', views.register_faculty, name='register_faculty'),
+    # /register/subject - Register Subject
     url(r'^subject/$', views.register_subject, name='register_subject'),
 
 ]
--- Registration/views.py
+++ Registration/views.py
@@ -1,14 +1,20 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 from .forms import StudentForm, FacultyForm, SubjectForm
 
 
 def register_student(request):
-    form = StudentForm()
-
+    if request.method == 'POST':
+        form = StudentForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/student/')
+    else:
+        form = StudentForm()
     return render(request, "register_student.html", {'form': form})
 
 
