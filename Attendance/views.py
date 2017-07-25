81ce48f3f541f3d2aedfba831b19551e55726f7a
r-sniper <rshahshah2890@gmail.com>
2017-07-26 02:39:48 +0530

    Just a commit

--- Attendance/views.py
+++ Attendance/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.http import HttpResponse
 from django.shortcuts import render
 
 from Registration.models import StudentDetails
@@ -12,3 +13,14 @@ def index(request):
     return render(request, "attendance.html", {
         'all_students': all_students
     })
+
+
+def save(request):
+    if request.method == 'POST':
+        print("Saving customers")
+        all_students = StudentDetails.objects.all().values_list('id')
+        for i in request.POST:
+            print(i)
+    else:
+        print("Not here because of post")
+    return HttpResponse("Here")
