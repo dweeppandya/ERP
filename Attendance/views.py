86a7116167b6dc5aec9284f602629d5e4934550f
r-sniper <rshahshah2890@gmail.com>
2017-07-26 01:10:59 +0530

    All students

--- Attendance/views.py
+++ Attendance/views.py
@@ -3,7 +3,12 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from Registration.models import StudentDetails
+
 
 # Create your views here.
 def index(request):
-    return render(request, "attendance.html")
+    all_students = StudentDetails.objects.all()
+    return render(request, "attendance.html", {
+        'all_students': all_students
+    })
