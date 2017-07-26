410277786d04b89cfc69fd8fcd134f1164f3d6a8
r-sniper <rshahshah2890@gmail.com>
2017-07-26 12:14:51 +0530

    Just a commit

--- Attendance/views.py
+++ Attendance/views.py
@@ -4,20 +4,23 @@ from __future__ import unicode_literals
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from Registration.models import StudentDetails
+from Registration.models import StudentDetails,Subject
 
 
 # Create your views here.
 def index(request):
     all_students = StudentDetails.objects.all()
+    all_subjects = Subject.objects.all()
     return render(request, "attendance.html", {
-        'all_students': all_students
+        'all_students': all_students,
+        'all_subjects':all_subjects
     })
 
 
 def save(request):
     if request.method == 'POST':
         print("Saving Student")
+
         # print(request.POST.get)
         # all_students = StudentDetails.objects.all().values_list('id')
         for i in request.POST:
